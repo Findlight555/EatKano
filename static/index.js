ac366cef404baff80ebd2f1338da6b2bbafd2261
yourshuidad <98597635+yourshuidad@users.noreply.github.com>
2022-01-28 22:20:21 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -441,11 +441,11 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
             SubmitResults();
         }
 
-        if (cps <= 5) return '试着好好练一下？';
-        if (cps <= 8) return 'TCL';
-        if (cps <= 10)  return 'TQL';
-        if (cps <= 15) return '您';
-        return '人？';
+        if (cps <= 5) return '你是对汤姐的爱不够吗？';
+        if (cps <= 8) return '寂寞汤姐需要您加油呢';
+        if (cps <= 10)  return '再加把劲，汤姐就是你的';
+        if (cps <= 15) return '汤姐爱你呦';
+        return '汤姐的爱';
     }
 
     function toStr(obj) {
